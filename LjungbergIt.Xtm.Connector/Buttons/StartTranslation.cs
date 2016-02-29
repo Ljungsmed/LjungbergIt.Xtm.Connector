90ad06d434ad9af9cd47d4d8becaa1cb63a08436
Morten Ljungberg <mlj@sitecore.net>
2016-02-29 16:38:06 +0100

    commit

---
+++ LjungbergIt.Xtm.Connector/Buttons/StartTranslation.cs
@@ -0,0 +1,17 @@
+﻿using Sitecore.Shell.Framework.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LjungbergIt.Xtm.Connector.Buttons
+{
+    class StartTranslation : Command
+    {
+        public override void Execute(CommandContext context)
+        {
+            Sitecore.Context.ClientPage.ClientResponse.Alert("Testing");
+        }
+    }
+}
